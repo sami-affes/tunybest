4a1383ddac615cb0854fc057d559979301aa26fe
talel-elghali <talelelghali96@gmail.com>
2020-10-21 16:52:26 +0100

    new components added

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const app = express()
 const bodyParser = require('body-parser')
 const db = require('./backend/database/index.js')
 
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/client/dist'))
 
+
 app.use('/user',require('./backend/routes/Users'))
 app.use('/comment',require('./backend/routes/Comments'))
 app.use('/video',require('./backend/routes/Videos.js'))
 app.use('/favorite',require('./backend/routes/Favorites.js'))
+
 
 const port = 3000
 app.listen(port, () => {
